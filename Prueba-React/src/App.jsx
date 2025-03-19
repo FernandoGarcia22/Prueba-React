3e1620d66af0d8e4acdc9b4d48af237ccedaa37d
FernandoGarcia22 <fernandogarciaguasch.fg@gmail.com>
2025-03-19 15:40:56 -0300

    adding project

---
+++ Prueba-React/src/App.jsx
@@ -0,0 +1,61 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+import Pizza from "./pages/Pizza";
+import Profile from "./pages/Profile";
+import NotFound from "./views/NotFound";
+import "./App.css";
+
+function App() {
+  return (
+    <UserProvider>
+      <CartProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+
+            {/* ✅ Si el usuario ya está autenticado, no puede acceder a /login ni /register */}
+            <Route
+              path="/register"
+              element={
+                <PublicRoute>
+                  <Register />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <PublicRoute>
+                  <Login />
+                </PublicRoute>
+              }
+            />
+
+            {/* ✅ Ruta protegida: Si no está autenticado, redirige a /login */}
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route path="/api/pizzas/:id" element={<Pizza />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      </CartProvider>
+    </UserProvider>
+  );
+}
+
+export default App;
